ffa4978ae257b79c9b234dd25a997748c5798166
Dennis Nguyen <dnsnguy08@gmail.com>
2022-07-12 17:59:57 -0700

    added helper methods for appending and reading from database

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require("path");
 const fs = require('fs');
+const crypto = require('crypto');
+const { readFromFile, readAndAppend } = require('./utils/fsUtils');
 
 // const routes = require('./routes');
 const app = express();
-const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+// const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
+// , (err, data) => {
+//     return res.status(400).json({err});
+// });
 
 
 
@@ -29,17 +34,35 @@ app.get('/', (req, res) => {
   });
 
 app.get('/api/notes', (req, res) => {
-    res.json(data);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    // const {note} = req.body;
+    // res.json(data);
 });
 
 app.get('/api/notes/:id', (req, res) => {
+    const { id } = req.body;
     console.log("I'M HIT!!!!!");
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    readFromFile('./db/db.json').then((id) => res.json(JSON.parse(id)));
 });
 
 app.post('/api/notes', (req, res) => {
-console.log("I'M HIT NOTES!!!");
-res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    console.log(`${req.method} request received to add note`);
+    console.log(req.body);
+    
+    const { title, text, id } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: crypto.randomBytes(2).toString("hex"),
+        };
+        readAndAppend(newNote, './db/db.json');
+    } else {
+        res.error('Error adding note');
+    }
+// const newNote = req.body;
+// data.push(newNote)
+// res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.delete('/api/', (req, res) => {
